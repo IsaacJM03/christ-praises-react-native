2de7c064cc13f80eac6d3234cd7c7cfc7998a3fc
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2025-07-21 12:01:22 +0300

    added user session handling

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,8 +1,25 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Stack } from 'expo-router'
+import { AuthProvider, useAuth } from '../contexts/AuthContext'
+import { supabase } from '../lib/supabase'
 
 const _layout = () => {
+  return (
+    <AuthProvider>
+      <MainLayout />
+    </AuthProvider>
+  )
+}
+const MainLayout = () => {
+  const {setAuth} = useAuth();
+
+  useEffect(() => {
+    supabase.auth.onAuthStateChange((event, session) => {
+      // setSession(session);
+      console.log('session user:', session?.user); 
+    })
+  })
   return (
     <Stack
       screenOptions={{
