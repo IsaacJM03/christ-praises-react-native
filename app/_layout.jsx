6cb6093e151658381ceacecbd4776083a4aff0e5
Isaac Mwesigwa <mwesigwaisaac40@gmail.com>
2025-07-29 11:14:08 +0300

    began logout button work

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
-import { Stack } from 'expo-router'
+import { Stack, useRouter } from 'expo-router'
 import { AuthProvider, useAuth } from '../contexts/AuthContext'
 import { supabase } from '../lib/supabase'
 
@@ -13,13 +13,21 @@ const _layout = () => {
 }
 const MainLayout = () => {
   const {setAuth} = useAuth();
+  const router = useRouter();
 
   useEffect(() => {
     supabase.auth.onAuthStateChange((event, session) => {
       // setSession(session);
       console.log('session user:', session?.user); 
+      if(session) {
+        setAuth(session?.user)
+        router.replace('/home'); 
+      } else {
+        setAuth(null);
+        router.replace('/welcome')
+      }
     })
-  })
+  },[])
   return (
     <Stack
       screenOptions={{
